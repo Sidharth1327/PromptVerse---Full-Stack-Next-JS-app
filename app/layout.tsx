5bf0bd1e5fde15bf062c09b6830f433d025690fb
Sidharth1327 <sidharthgnair1327@gmail.com>
2024-06-24 21:29:09 +0530

    Created Nav Bar, layout.jsx, page.jsx

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import '../app/globals.css';
+import Nav from '../components/Nav';
+import '../components/Provider';
+
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav/>
+          {children}
+        </main>
+      
+    </body>
+  </html>
+);
+
+export default RootLayout;
